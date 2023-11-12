d402d6d433affa2074c8ee310b3b9f545b108683
Md Ismail Hossain <shojeeb.aust@gmail.com>
2023-11-12 18:27:12 +0100

    changed machine name to fog cloud edge

--- three_layer/middleware.py
+++ three_layer/middleware.py
@@ -3,29 +3,29 @@ import requests
 
 app = Flask(__name__)
 
-def communicate_with_program_2(data):
-    # logic to send data to Program 2
-    response = requests.post("http://localhost:5001/communicate_with_program_2", json={"data": data})
+def communicate_with_fog(data):
+    # logic to send data to fog
+    response = requests.post("http://localhost:5001/communicate_with_fog", json={"data": data})
     return response.json()["result"]
 
-def communicate_with_program_3(data):
-    # logic to send data to Program 3
-    response = requests.post("http://localhost:5002/communicate_with_program_3", json={"data": data})
+def communicate_with_cloud(data):
+    # logic to send data to cloud
+    response = requests.post("http://localhost:5002/communicate_with_cloud", json={"data": data})
     return response.json()["result"]
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
-    # initialize a variable with data from program 1
-    data_from_program_1 = request.json['data']
+    # initialize a variable with data from Edge
+    data_from_edge = request.json['data']
     
-    # Send data to Program 2 and receive the computed data in a variable
-    processed_data_2 = communicate_with_program_2(data_from_program_1)
+    # Send data to Fog and receive the computed data in a variable
+    processed_data_fog = communicate_with_fog(data_from_edge)
     
-    # Send processed data from program 2 to Program 3
-    processed_data_3 = communicate_with_program_3(processed_data_2)
+    # Send processed data from Fog to Cloud
+    processed_data_cloud = communicate_with_cloud(processed_data_fog)
     
     # Return final processed data to Program 1
-    return jsonify({"result": processed_data_3})
+    return jsonify({"result": processed_data_cloud})
 
 if __name__ == '__main__':
     app.run(port=5000)
--- three_layer/programe2.py
+++ three_layer/programe2.py
@@ -1,21 +1,21 @@
 # program2.py
 from flask import Flask, jsonify, request
 
-def perform_work_on_data_2(data):
+def perform_work_on_data_fog(data):
     # logic to process data in Program 2
-    return f"Processed by Program 2: {data.upper()}"
+    return f"Processed by fog: {data.upper()}"
 
 app = Flask(__name__)
 
-@app.route('/communicate_with_program_2', methods=['POST'])
-def communicate_with_program_2():
+@app.route('/communicate_with_fog', methods=['POST'])
+def communicate_with_fog():
     data_from_middleware = request.json['data']
     
     # Perform some computation on the data
-    processed_data_2 = perform_work_on_data_2(data_from_middleware)
+    processed_data_fog = perform_work_on_data_fog(data_from_middleware)
     
     # Return processed data to the middleware
-    return jsonify({"result": processed_data_2})
+    return jsonify({"result": processed_data_fog})
 
 if __name__ == '__main__':
     app.run(port=5001)
