635c18a8d62a7835f1de7d9cb3db9967045a6fe2
Md Ismail Hossain <shojeeb.aust@gmail.com>
2023-11-13 11:01:26 +0100

    a

--- three_middleware/programe3.py
+++ three_middleware/programe3.py
@@ -1,21 +1,7 @@
 # program3.py
-from flask import Flask, jsonify, request
 
 def perform_work_on_data_cloud(data):
     # logic to process data in cloud
     return f"Processed by cloud: {data}"
 
-app = Flask(__name__)
 
-@app.route('/communicate_with_cloud', methods=['POST'])
-def communicate_with_cloud():
-    data_from_middleware = request.json['data']
-    
-    # Perform some computation on the data
-    processed_data_cloud = perform_work_on_data_cloud(data_from_middleware)
-    
-    # Return processed data to the middleware
-    return jsonify({"result": processed_data_cloud})
-
-if __name__ == '__main__':
-    app.run(port=5002)
