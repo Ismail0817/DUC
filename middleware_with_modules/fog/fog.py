ccf84d06e42b43faee747288724a8bbc29d3e46e
Md Ismail Hossain <shojeeb.aust@gmail.com>
2024-01-03 20:25:14 +0100

    edge fog cloud

--- middleware_with_modules/fog/fog.py
+++ middleware_with_modules/fog/fog.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import requests
+import time
 
 class Fog:
     def __init__(self, host, port):
@@ -35,3 +37,36 @@ class Fog:
             print(f"Error: {e}")
         finally:
             self.server_socket.close()
+            self.commmunicate_with_cloud()
+            
+
+    def commmunicate_with_cloud(self):
+        # negotiation and orchetration
+        response_from_middleware = requests.post("http://localhost:5000/api_fog", json={"function": "deploy_in_cloud", "name": "process1"})
+        print(response_from_middleware.json()["result_negotiate"])
+
+        # socket open
+        response_from_middleware = requests.post("http://localhost:5000/api_fog", json={"function": "open_socket", "name": "process1"})
+        print(response_from_middleware.json()["result"])
+        host = response_from_middleware.json()["host"]
+        port = response_from_middleware.json()["port"]
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            client_socket.connect((host, port))
+            print(f"Connected to middleware at {host}:{port}")
+
+            counter = 1
+            while counter < 6:
+                data_to_send = f"Data: {counter}"
+                client_socket.sendall(data_to_send.encode('utf-8'))
+                print(f"Sent: {data_to_send}")
+
+                time.sleep(2)
+                counter += 1
+
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            client_socket.close()
